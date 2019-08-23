67c778adbb8bd675a56ea6b5f7b2a98d0bcec2f5
Sudesh Pamidi <sudesh.pamidi@gmail.com>
2019-08-23 15:22:07 -0400

    Review Post request is completed

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -55,26 +55,60 @@ $(document).ready(function() {
     function getService(serviceId) {
 
         $.getJSON(`api/services/${serviceId}`, (service) => {
-            console.log(service);
-
             $(".card .card-img-top").attr("src", "images/doubleliftpurebliss.jpg");
             $(".card .card-title").html(service.ServiceName);
             $(".card h4").html("$" + Number(service.Price).toFixed(2));
             $(".card .card-text").html(service.Description);
 
+            $("#card-review").empty();
+
             $.each(service.Reviews, (index, review) => {
-                console.log(review);
-                $("#card-review .card-body").prepend($("<hr>"));
-                $("#card-review .card-body").prepend($("<small />")
+                $("#card-review").prepend($("<hr>"));
+                $("#card-review ").prepend($("<small />")
                     .html('Posted by ' + review.PostedBy + ' ' + review.Date)
                     .attr("class", "text-muted")
                 );
-                $("#card-review .card-body").prepend($("<p />").html(review.Description));
-
+                $("#card-review").prepend($("<p />").html(review.Description));
             });
 
+            let textArea = `<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">            
+                <textarea rows="4" cols="100" id="reviewArea"></textarea>            
+            </div>`;
+
+            $("#card-review").append(textArea);
+
+            $("#card-review").append($("<button />")
+                .attr("class", "btn btn-success")
+                .attr("type", "button")
+                //.attr("data-toggle", "collapse")
+                //.attr("data-target", "#collapseOne")
+                .html("Leave a Review")
+                .on("click", (e) => {
+                    e.preventDefault();
+                    addReview(service.ServiceID);
+                })
+            );
             // $("#productCard").show();
-        })
+        });
+
+        function addReview(serviceid) {
+            let url = "/api/review";
+            let postData = "&description=" + $("#reviewArea").val();
+            postData = postData + "&postedby=" + "Anonymous";
+            postData = postData + "&serviceid=" + serviceid;
+
+            if ($("#reviewArea").val().trim() != "") {
+                $.ajax({
+                        url: url,
+                        type: "POST",
+                        data: postData
+                    })
+                    .done(function() {
+                        getService(serviceid);
+                    });
+            };
+        };
     };
+
 
 });
